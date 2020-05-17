67f9a4a542ef05592454ca71b461e72c785e0e43
Brian Krabec <briankrabec@Brians-MacBook-Pro.local>
2020-05-16 19:45:13 -0500

    Setting up Flask for deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, jsonify, g
 
 from resources.users import users
 
@@ -70,7 +71,30 @@ app.register_blueprint(events, url_prefix='/api/events/')
 app.register_blueprint(statuses, url_prefix='/api/statuses/')
 app.register_blueprint(attendances, url_prefix='/api/attendances/')
 
+@app.before_request # use this decorator to cause a function to run before reqs
+def before_request():
+  """Connect to the db before each request"""
+  # store the database as a global var in g
+  print("you should see this before each request") # optional -- to illustrate that this code rus before each request -- similar to custom middleware in express.  you could also set it up for specific blueprints only.
+  g.db = models.DATABASE
+  g.db.connect()
 
+
+@app.after_request # use this decorator to cause a function to run after reqs
+def after_request(response):
+  """Close the db connetion after each request"""
+  print("you should see this after each request") # optional -- to illustrate that this code runs after each request
+  g.db.close()
+  return response # go ahead and send response back to client 
+                  # (in our case this will be some JSON)
+
+
+
+# ADD THESE THREE LINES -- because we need to initialize the
+# tables in production too!
+if 'ON_HEROKU' in os.environ: 
+  print('\non heroku!')
+  models.initialize()
 
 
 if __name__ == '__main__':
